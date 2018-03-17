2c01f7f733c8210109aca70510542091420234f6
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-03-17 11:58:47 +0530

    Create average.py

---
+++ average.py
@@ -0,0 +1,7 @@
+num=int(input("How many numbers:"))
+total_sum=0
+for n in range(num):
+  numbers=flot(input("Enter numbr"))
+  total_sum+=numbers
+avg=totl_sum/num
+print("average of",num"number is",avg)
