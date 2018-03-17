33215d470c0d221677b9c48659ec21f48903fd96
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-03-17 14:27:05 +0530

    Create countstr.py

---
+++ countstr.py
@@ -0,0 +1,11 @@
+string=raw_input("Enter string:")
+count1=0
+count2=0
+for i in string:
+    if(i.isdigit()):
+        count1=count1+1
+    count2=cunt2+1
+print("the number of digits is")
+print(count1)
+print("the number of character is:")
+print(count2)
