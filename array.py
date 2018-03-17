3a1b5057ad5a4021bb7504dbed8ed1eaef7bd45d
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-03-17 09:43:52 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,6 @@
+lst=[]
+num=int(input("how many numbers:"))
+for n in range(num):
+  numbers=int(input("Enter number")
+  lst.append(numbers)
+  print("maximum element in the list is:",max(lst),"\nMinimum element in th list is:",min(lst))
