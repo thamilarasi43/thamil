2cd7bcf6ad8050f6ad406605d981cd6cad345ecd
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-03-17 15:13:46 +0530

    Create nearest.py

---
+++ nearest.py
@@ -0,0 +1,8 @@
+def find_closest(A, target):
+    #A must be sorted
+    idx = A.searchsorted(target)
+    idx = np.clip(idx, 1, len(A)-1)
+    left = A[idx-1]
+    right = A[idx]
+    idx -= target - left < right - target
+    return idx
