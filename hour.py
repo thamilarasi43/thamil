c46c4677dfbd776081b1e073a8b439c3eb651c0a
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-03-17 11:05:52 +0530

    Create hour.py

---
+++ hour.py
@@ -0,0 +1,6 @@
+days=int(input("Input dys"))*3600*24
+hour=int(input("Input hour"))*3600
+minute=int(input("Input minutes"))*60
+seconds=int(input("seconds"))
+time=days+hour+minute+second
+print("the amount of seonds",time)
